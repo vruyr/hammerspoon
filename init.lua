d373dbe7d1b8ba01b9d88cfa063f6fae1f662c44
Vruyr Gyolchanyan <vruyr@vruyr.com>
2025-07-27 11:24:13 -0400

    Added hotkeys for moving and maximizing windows.

--- init.lua
+++ init.lua
@@ -1,3 +1,11 @@
 hs.loadSpoon("ScreensAndWindows")
 
 spoon.ScreensAndWindows:startScreenWatcher()
+
+hs.hotkey.bind({"cmd", "ctrl"}, "A", function()
+	spoon.ScreensAndWindows:maximizeFrontmostWindowOnScreenWithMouse()
+end)
+
+hs.hotkey.bind({"cmd", "ctrl"}, "Z", function()
+	spoon.ScreensAndWindows:moveFrontmostWindowToMousePosition()
+end)
